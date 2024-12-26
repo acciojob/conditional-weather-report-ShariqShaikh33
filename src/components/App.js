a3f9636235c4994a02c7654924f677df6a5ee175
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-12-26 15:54:36 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
+
 
 const App = () => {
+  let defaultinput = { temperature: 0, conditions: "" };
+  const [WeatherData, setWeatherData] = useState(defaultinput);
+
+  useEffect(()=>{
+    setWeatherData({ temperature: 25, conditions: "Sunny" });
+  },[]);
+
+  console.log(WeatherData.temperature);
+  console.log(WeatherData.conditions);
+
   return (
     <div>
+      <WeatherDisplay WeatherData={WeatherData}/>
         {/* Do not remove the main div */}
     </div>
   )
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/WeatherDisplay.css"
+
+const WeatherDisplay =({WeatherData})=>{
+    const temperatureStyle = {
+        color: WeatherData.temperature > 20 ? 'red' : 'blue'
+    };
+    return (
+        <div className="mainWrapper">
+            <div className="TemperatureDiv">
+                <div className="tempText">Temperature</div>
+                
+                <p style={temperatureStyle} className="value">{WeatherData.temperature}</p>
+            </div>
+            <div className="ConditionDiv">
+                <div className="condText">Conditions</div>
+                <p style={temperatureStyle} className="value">{WeatherData.conditions}</p>
+            </div>
+        </div>
+    )
+}
+
+export default WeatherDisplay;
